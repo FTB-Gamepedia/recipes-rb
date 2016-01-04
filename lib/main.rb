d51bd172c3a6b17d347fb7d7e745b7ca901f9b8c
Chocohead <lo9tt@hotmail.com>
2016-01-04 19:47:22 +0000

    Squish together previous comits

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,26 @@
 require_relative 'recipe_parsers/shaped_ore'
 
-recipe = ARGV[0]
+def go(recipe)
+  if recipe =~ /^GameRegistry\.addRecipe\(new ShapedOreRecipe\(/
+    puts RecipeParsers::ShapedOreRecipeParser.new(recipe).parse
+    puts ""
+  else
+    puts 'That recipe type is not currently supported. Please submit a PR so we can support it.\n'
+  end
+end
 
-if recipe =~ /^GameRegistry\.addRecipe\(new ShapedOreRecipe\(/
-  maker = RecipeParsers::ShapedOreRecipeParser.new(recipe)
-  out = maker.parse
-  puts out
-  puts 'Done!'
+if ARGV[0].nil?
+  if !File.exists?('recipes.txt')
+    File.open('recipes.txt', 'w').close() #Kindly create it
+  else
+    file = File.open('recipes.txt', 'r')
+    while !file.eof?
+      go(file.readline)
+    end
+    file.close()
+    puts 'Done!'
+  end
 else
-  puts 'That recipe type is not currently supported. Please submit a PR or open an issue so we can support it.'
+  go(ARGV[0])
+  puts 'Done!'
 end
